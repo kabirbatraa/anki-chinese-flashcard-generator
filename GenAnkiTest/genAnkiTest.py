906a044dfd2c4be6be20b9c94198a80a9f3e6e31
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2024-03-14 16:55:53 -0700

    attempt to write basic genanki test

---
+++ GenAnkiTest/genAnkiTest.py
@@ -0,0 +1,39 @@
+
+import genanki 
+
+# how to escape html:
+# fields=[html.escape(f) for f in ['AT&T was originally called', 'Bell Telephone Company']]
+
+
+# how to create a model:
+my_model = genanki.Model(
+    1607392319,
+    "Simple Model",
+    fields=[
+        {"name": "Question"},
+        {"name": "Answer"},
+    ],
+    templates=[
+        {
+            "name": "Card 1",
+            "qfmt": "{{Question}}",
+            "afmt": '{{FrontSide}}<hr id="answer">{{Answer}}',
+        },
+    ],
+)
+# use a unique model id:
+# import random; random.randrange(1 << 30, 1 << 31)
+# and hardcode it into your Model definition.
+
+# create a new note:
+my_note = genanki.Note(
+    model=my_model, 
+    fields=["Capital of Argentina", "Buenos Aires"]
+)
+
+
+# make a deck
+my_deck = genanki.Deck(2059400110, "Country Capitals")
+
+my_deck.add_note(my_note)
+genanki.Package(my_deck).write_to_file('output.apkg')
